06ea8aeffe3c9f893410a4dbd54748030d0cfb22
aan-cloud <evirsanti@gmail.com>
2025-02-08 19:23:01 +0800

    feat: get merchant by category

--- app/request/merchants.ts
+++ app/request/merchants.ts
@@ -1,5 +1,20 @@
 import merchants from "../data/merchants.json";
 
-export const getAllMerchants = () => {
-    return merchants;
+export const getAllMerchants = (take? : number) => {
+    let merchantsTakes;
+
+    if (take) {
+        for (let index = 0; index < take; index++) {
+            
+        }
+    }
+    return merchantsTakes;
+};
+
+export const getMerchantByCategory = (categoryName: string) => {
+    const filteredMerchants = merchants.filter((merchant) => {
+        merchant.category === categoryName
+    });
+
+    return filteredMerchants;
 };
